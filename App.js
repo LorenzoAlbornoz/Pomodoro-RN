368a42aa4fd19b8275efab41de06e0a211476cc1
Lorenzo Albornoz <lorenzoa.28.la@gmail.com>
2024-05-16 09:16:27 -0300

    finalizacion pomodoro

--- App.js
+++ App.js
@@ -1,20 +1,98 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import {
+  StyleSheet,
+  Platform,
+  Text,
+  View,
+  SafeAreaView,
+  TouchableOpacity,
+} from "react-native";
+import { useEffect, useState } from "react";
+import Header from "./components/Header";
+import Timer from "./components/Timer";
+import { Audio } from "expo-av";
 
-export default function App() {
+const colors = ["#F7DC6F", "#A2D9CE", "#D7BDE2"];
+
+export default function Page() {
+  const [isWorking, setIsWorking] = useState(false);
+  const [time, setTime] = useState(25 * 60);
+  const [currentTime, setCurrentTime] = useState("POMO" | "SHORT" | "BREAK");
+  const [isActive, setIsActive] = useState(false);
+
+  useEffect(() => {
+    let interval = null;
+    if(isActive){
+      // run timer
+      interval = setInterval(() => {
+       setTime(time - 1)
+      }, 1000)
+    } else{
+      // clear interval
+      clearInterval(interval);
+    }
+
+    if(time === 0){
+      setIsActive(false)
+      setIsWorking((prev) => !prev)
+      setTime(isWorking ? 300 : 1500);
+    }
+    return () => clearInterval(interval)
+  }, [isActive, time])
+
+  function handleStartStop(){
+    playSound();
+    setIsActive(!isActive);
+  }
+
+  async function playSound(){
+    const { sound } = await Audio.Sound.createAsync(
+      require("./assets/mouse-click-117076.mp3")
+    )
+    await sound.playAsync();
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView
+      style={[styles.container, { backgroundColor: colors[currentTime] }]}
+    >
+      <View
+        style={{
+          flex: 1,
+          paddingHorizontal: 15,
+          paddingTop: Platform.OS === "android" && 30,
+          borderWidth: 3,
+        }}
+      >
+        <Text style={styles.title}>Pomodoro</Text>
+        <Header
+          currentTime={currentTime}
+          setCurrentTime={setCurrentTime}
+          setTime={setTime}
+        />
+        <Timer time={time} />
+        <TouchableOpacity onPress={handleStartStop} style={styles.button}>
+          <Text style={{ color: "white", fontWeight: "bold" }}>
+            {isActive ? "STOP" : "START"}
+          </Text>
+        </TouchableOpacity>
+      </View>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  },
+  title: {
+    fontSize: 32,
+    fontWeight: "bold",
+  },
+  button: {
+    alignItems: "center",
+    backgroundColor: "#333333",
+    padding: 15,
+    marginTop: 15,
+    borderRadius: 15,
   },
 });
